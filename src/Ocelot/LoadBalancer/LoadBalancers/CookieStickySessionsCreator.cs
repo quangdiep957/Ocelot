80bde0383f98ab78d3eddffe6f18e8504d329590
Eli Syrota <Eli.Syrota@simcorp.com>
2020-12-29 05:45:14 -0500

    Dependency Injection for IStickySessionStorage prodiver

--- src/Ocelot/LoadBalancer/LoadBalancers/CookieStickySessionsCreator.cs
+++ src/Ocelot/LoadBalancer/LoadBalancers/CookieStickySessionsCreator.cs
@@ -10,13 +10,19 @@ namespace Ocelot.LoadBalancer.LoadBalancers
 {
     public class CookieStickySessionsCreator : ILoadBalancerCreator
     {
+        private readonly IStickySessionStorage _sessionStorage;
+
+        public CookieStickySessionsCreator(IStickySessionStorage sessionStorage)
+        {
+            _sessionStorage = sessionStorage;
+        }
+
         public Response<ILoadBalancer> Create(DownstreamRoute route, IServiceDiscoveryProvider serviceProvider)
         {
             var loadBalancer = new RoundRobin(async () => await serviceProvider.Get());
             var bus = new InMemoryBus<StickySession>();
-            var storage = new InMemoryStickySessionStorage();
             return new OkResponse<ILoadBalancer>(new CookieStickySessions(loadBalancer, route.LoadBalancerOptions.Key,
-                route.LoadBalancerOptions.ExpiryInMs, bus, storage));
+                route.LoadBalancerOptions.ExpiryInMs, bus, _sessionStorage));
         }
 
         public string Type => nameof(CookieStickySessions);
