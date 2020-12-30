f31f59d6828e77ac03d74c71cf1665153d7712f4
Eli Syrota <Eli.Syrota@simcorp.com>
2020-12-29 21:30:40 -0500

    Refactoring

--- src/Ocelot/DependencyInjection/OcelotBuilder.cs
+++ src/Ocelot/DependencyInjection/OcelotBuilder.cs
@@ -201,7 +201,7 @@ namespace Ocelot.DependencyInjection
             AddCustomLoadBalancer((provider, route, serviceDiscoveryProvider) => new T());
             return this;
         }
-
+        
         public IOcelotBuilder AddCustomLoadBalancer<T>(Func<T> loadBalancerFactoryFunc)
             where T : ILoadBalancer
         {
@@ -209,7 +209,6 @@ namespace Ocelot.DependencyInjection
                 loadBalancerFactoryFunc());
             return this;
         }
-
         public IOcelotBuilder AddCustomLoadBalancer<T>(Func<IServiceProvider, T> loadBalancerFactoryFunc) 
             where T : ILoadBalancer
         {
@@ -297,8 +296,8 @@ namespace Ocelot.DependencyInjection
 
             Services.Replace(ServiceDescriptor.Describe(
                 typeof(IPlaceholders),
-                s => (IPlaceholders)objectFactory(s,
-                    new[] { CreateInstance(s, wrappedDescriptor)}),
+                s => (IPlaceholders) objectFactory(s,
+                    new[] {CreateInstance(s, wrappedDescriptor)}),
                 wrappedDescriptor.Lifetime
             ));
 
