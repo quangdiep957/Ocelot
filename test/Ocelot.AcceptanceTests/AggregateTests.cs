859674346d7958bf2ea70dc39a777f4c3b9bba1d
raman-m <dotnet044@gmail.com>
2023-08-22 15:00:56 +0300

    Add required namespace

--- test/Ocelot.AcceptanceTests/AggregateTests.cs
+++ test/Ocelot.AcceptanceTests/AggregateTests.cs
@@ -6,7 +6,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Http;
-
+using Newtonsoft.Json;
 using Ocelot.Configuration.File;
 using Ocelot.Middleware;
 using Ocelot.Multiplexer;
