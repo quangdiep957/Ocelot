2d2bdaa4b7152aa1c300f473539b37658604c52f
Raynald Messié <redbird_project@yahoo.fr>
2024-05-29 16:04:46 +0200

    #2080 Adds parameters (in this case those of Polly V8) to fine-tune circuit-breaker behavior

--- src/Ocelot.Provider.Polly/PollyQoSResiliencePipelineProvider.cs
+++ src/Ocelot.Provider.Polly/PollyQoSResiliencePipelineProvider.cs
@@ -81,8 +81,8 @@ public class PollyQoSResiliencePipelineProvider : IPollyQoSResiliencePipelinePro
         var info = $"Circuit Breaker for Route: {GetRouteName(route)}: ";
         var strategyOptions = new CircuitBreakerStrategyOptions<HttpResponseMessage>
         {
-            FailureRatio = 0.8,
-            SamplingDuration = TimeSpan.FromSeconds(10),
+            FailureRatio = options.FailureRatio,
+            SamplingDuration = TimeSpan.FromSeconds(options.SamplingDuration),
             MinimumThroughput = options.ExceptionsAllowedBeforeBreaking,
             BreakDuration = options.DurationOfBreak > QoSOptions.LowBreakDuration
                 ? TimeSpan.FromMilliseconds(options.DurationOfBreak)
